ee4b48477985051843316e7d6ea2319956d07887
surajdwivedi1997 <47377479+surajdwivedi1997@users.noreply.github.com>
2025-09-23 23:14:02 +0530

    Delete prgi-dashboard directory

--- prgi-dashboard/src/main/java/com/example/prgi/repo/NewRegDeficientApplicationRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.prgi.repo;
-
-import com.example.prgi.domain.NewRegDeficientApplication;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NewRegDeficientApplicationRepository extends JpaRepository<NewRegDeficientApplication, Long> {}
--- prgi-dashboard/src/main/java/com/example/prgi/repo/NewRegNewApplicationRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.prgi.repo;
-
-import com.example.prgi.domain.NewRegNewApplication;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NewRegNewApplicationRepository extends JpaRepository<NewRegNewApplication, Long> {}
