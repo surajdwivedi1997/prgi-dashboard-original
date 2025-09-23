a0985c42abfb9378a48c18c65edb94522b821572
surajdwivedi1997 <47377479+surajdwivedi1997@users.noreply.github.com>
2025-09-23 23:06:26 +0530

    Add files via upload

---
+++ prgi-dashboard/src/main/java/com/example/prgi/repo/NewRegNewApplicationRepository.java
@@ -0,0 +1,6 @@
+package com.example.prgi.repo;
+
+import com.example.prgi.domain.NewRegNewApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NewRegNewApplicationRepository extends JpaRepository<NewRegNewApplication, Long> {}
